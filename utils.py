67ccfbb5fed9f1ba976f5fd6be510c9ddfbddef3
HarlockOfficial <harlockofficial.github@gmail.com>
2024-03-26 15:02:38 +0100

    [fix] to and from categorical methods

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
 import numpy as np
 
 
-def to_categorical(labels: list[str]):
+def to_categorical(labels: list[str]|str):
     labels_to_numbers = {
         'feet': 0,
         'left_hand': 1,
         'right_hand': 2,
     }
+    if isinstance(labels, str):
+        return labels_to_numbers[labels]
     return np.array([labels_to_numbers[label] for label in labels])
 
-def from_categorical(labels: list[int]):
+def from_categorical(labels: list[int]|int):
     numbers_to_labels = {
         0: 'feet',
         1: 'left_hand',
         2: 'right_hand',
     }
+    if isinstance(labels, int):
+        return numbers_to_labels[labels]
     return [numbers_to_labels[label] for label in labels]
